879c82a34db36dc3cc2c490b497dc5d4ac0fde13
Danilo Shiroma <danilo.shiroma@gmail.com>
2022-03-08 16:37:55 -0400

    feat: puxa dados do prismic e cria estilizacao da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +27,69 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={commonStyles.container}>
+        <section className={styles.postsSection}>
+          <img src="Logo.svg" alt="logo" />
+          {postsPagination.results.map(post => (
+            <div className={styles.singlePost}>
+              <h2>{post.data.title}</h2>
+              <h4>{post.data.subtitle}</h4>
+              <div className={styles.postInfo}>
+                <FiCalendar color="#BBBBBB" />
+                <p>{post.first_publication_date}</p>
+                <FiUser color="#BBBBBB" />
+                <p>{post.data.author}</p>
+              </div>
+            </div>
+          ))}
+        </section>
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'posts'),
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 1,
+    }
+  );
+  const { next_page } = postsResponse;
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results,
+        next_page,
+      },
+    },
+    revalidate: 60 * 60,
+  };
+};
